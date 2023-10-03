016d49156d3bc8c4dad002b53661c4b0539ed8db
Martin Lukáč <xlukacm3@stuba.sk>
2023-10-03 14:32:50 +0200

    project initialization

---
+++ src/main/java/com/example/dp_spring_backend/DpSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dp_spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dp_spring_backend/DpSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dp_spring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
